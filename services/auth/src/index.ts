b2f770a0fea98796888165d512c55c6115bb3302
NhanNT-SE <nhannt.se1905@gmail.com>
2021-07-11 12:45:03 +0700

    modile docker file and docker compose file

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -4,14 +4,11 @@ import { app } from "./app";
 const PORT = 3000 || process.env.PORT;
 // CONNECT DATABASE
 (async function () {
- const MONGO_URI = "mongodb://mongo:27017/t-stores"
-//   if (!process.env.MONGO_URI) {
-//     throw new Error("MONGO_URI must be define");
-//   }
- 
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be define");
+  }
   try {
-  
-    await connect(MONGO_URI, {
+    await connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
