7114031979ec5bc118414926af4f6c1ab85ed08f
NhanNT-SE <nhannt.se1905@gmail.com>
2021-07-12 21:57:29 +0700

    add config deploy for auth service

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -2,13 +2,13 @@ import { connect } from "mongoose";
 import { app } from "./app";
 
 const PORT = 3000 || process.env.PORT;
-// CONNECT DATABASE
 (async function () {
   if (!process.env.MONGO_URI) {
     throw new Error("MONGO_URI must be define");
   }
+  const stringConnect = process.env.MONGO_URI || "mongodb://localhost/t-stores"
   try {
-    await connect(process.env.MONGO_URI, {
+    await connect(stringConnect, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
