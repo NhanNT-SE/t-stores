1c635094d16a9dc8ef29d977afebc8de60a3f96f
NhanNT-SE <nhannt.se1905@gmail.com>
2021-07-30 21:02:28 +0700

    Updates common module

--- common/src/middleware/require-auth.ts
+++ common/src/middleware/require-auth.ts
@@ -1,9 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 import { UnauthorizedError } from "../errors";
 const requireAuth = async (req: Request, res: Response, next: NextFunction) => {
-  if (!req.currentUser) {
-    throw new UnauthorizedError();
+  try {
+    if (!req.currentUser) {
+      throw new UnauthorizedError();
+    }
+    next();
+  } catch (error) {
+    next(error)
   }
-  next();
 };
 export { requireAuth };
