337683e8116181d2455914614d376308c120f89e
NhanNT-SE <nhannt.se1905@gmail.com>
2021-08-02 20:59:12 +0700

    Updates common module

--- common/src/helpers/otp-helper.ts
+++ common/src/helpers/otp-helper.ts
@@ -1,6 +1,9 @@
 import qrcode from "qrcode";
 import { authenticator } from "otplib";
-
+import { ISecretEncrypt } from "../interfaces";
+const crypto = require("crypto");
+const algorithm = "aes-256-ctr";
+const iv = crypto.randomBytes(16);
 const generateQRCode = async (
   username: string,
   serviceName: string,
@@ -15,15 +18,45 @@ const generateQRCode = async (
   }
 };
 
-const generateSecretOTP = () => {
-  return authenticator.generateSecret();
+const encryptSecretOTP = (secretKey: string): ISecretEncrypt => {
+  const secretOTP = authenticator.generateSecret();
+  const key_in_bytes = crypto
+    .createHash("sha256")
+    .update(String(secretKey))
+    .digest("base64")
+    .substr(0, 32);
+  const cipher = crypto.createCipheriv(algorithm, key_in_bytes, iv);
+  const encrypted = Buffer.concat([cipher.update(secretOTP), cipher.final()]);
+  return {
+    iv: iv.toString("hex"),
+    content: encrypted.toString("hex"),
+  };
+};
+const decryptSecretOTP = (hash: any, secretKey: string) => {
+  const key_in_bytes = crypto
+    .createHash("sha256")
+    .update(String(secretKey))
+    .digest("base64")
+    .substr(0, 32);
+  const decipher = crypto.createDecipheriv(
+    algorithm,
+    key_in_bytes,
+    Buffer.from(hash.iv, "hex")
+  );
+
+  const decrpyted = Buffer.concat([
+    decipher.update(Buffer.from(hash.content, "hex")),
+    decipher.final(),
+  ]);
+  return decrpyted.toString();
 };
 const verifyOTPToken = (token: string, secret: string) => {
   return authenticator.verify({ token, secret });
 };
-export { generateQRCode, generateSecretOTP, verifyOTPToken };
+export { generateQRCode, encryptSecretOTP, verifyOTPToken };
 export const OTPHelper = {
   generateQRCode,
-  generateSecretOTP,
-  verifyOTPToken
-}
+  encryptSecretOTP,
+  decryptSecretOTP,
+  verifyOTPToken,
+};
---
+++ common/src/interfaces/secret-encrypt.ts
@@ -0,0 +1,4 @@
+export interface ISecretEncrypt {
+  iv: string;
+  content: string;
+}
