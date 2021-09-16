9c46bbcfac5eb569497aedbaffa053b885830c16
NhanNT-SE <nhannt.se1905@gmail.com>
2021-09-16 16:37:53 +0700

    Updates common module

--- common/src/middleware/current-user.ts
+++ common/src/middleware/current-user.ts
@@ -3,21 +3,14 @@ import { JwtHelper, RedisHelper } from '../helpers';
 import { CurrentUser } from '..';
 
 export const currentUser = async (req: Request, res: Response, next: NextFunction) => {
-  console.log("current user start")
-
   try {
-    console.log("current user check enviroment", process.env.COOKIE_ACCESS_TOKEN)
     if (!process.env.COOKIE_ACCESS_TOKEN) {
       return next();
     }
     const accessToken = req.cookies[process.env.COOKIE_ACCESS_TOKEN];
-    console.log("current user check accesstoken", accessToken)
-
     if (!accessToken) {
       return next();
     }
-    console.log("start decoded")
-
     const decoded = JwtHelper.verifyToken(
       accessToken,
       process.env.ACCESS_TOKEN_SECRET!
@@ -29,7 +22,6 @@ export const currentUser = async (req: Request, res: Response, next: NextFunctio
     if (!tokenRedis || tokenRedis !== accessToken) {
       return next();
     }
-    console.log("current user", decoded)
     req.currentUser = decoded;
     return next();
   } catch (error) {
